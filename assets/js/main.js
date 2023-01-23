1eaca693542d5bfa400ad85f146ca4c7743f81f2
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-23 18:21:15 +0100

    start

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+console.log("klappts?");
+
+/* 
+Frage
+
+per input verschiedene Formen von Farben in js übergeben
+1. #22222
+2. pink 
+3. rgb(255, 0, 0)
+4. rgba(255, 0, 0, 0.4)
+
+auf button clicken
+
+dann wird hintergrundfarbe verändert
+
+*/
